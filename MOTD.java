6abf77db0c813230858291b244f62c77a5fd229b
only nonsense <130183311+onlynonsense@users.noreply.github.com>
2023-04-07 23:31:44 +0200

    Update and rename SpigotMOTD.java to MOTD.java

--- MOTD.java
+++ MOTD.java
@@ -1,17 +1,14 @@
-package org.nonsense.spigotmotd;
+package org.nonsense.motd;
 
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SpigotMOTD extends JavaPlugin {
+public final class MOTD extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // events
         Bukkit.getServer().getPluginManager().registerEvents(new SpigotMOTDServerListPing(this), this);
 
-
-        // config
         getConfig().options().copyDefaults();
         saveDefaultConfig();
 
